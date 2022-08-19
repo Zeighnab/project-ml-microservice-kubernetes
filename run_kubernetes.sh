abebb7e0f319a8eee91fa001c87d815e95228a22
EC2 Default User <ec2-user@ip-172-31-18-133.ec2.internal>
2022-08-19 10:23:41 +0000

    final changes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="zeighnab/app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy kube-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/$(echo $(kubectl get pods) | awk -F ' ' '{print $6}') --address 0.0.0.0 8000:80
